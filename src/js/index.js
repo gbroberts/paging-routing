6e4b48a6e44ccbd44c936a9e70557dbf59a7b96a
Graham Roberts <grahamroberts31@gmail.com>
2021-10-27 15:59:51 -0600

    Adds creation of store to onAppInit function in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 import Router from "./routes/router";
 import { dataFetcher } from "./utils/dataFetcher";
+import { createStore } from "./redux/store";
 
 
 const onAppInit = async function(e){
     let employees = await dataFetcher('./data/todo.json')
 
-    // createStore(employees)
+    createStore(employees)
     Router(window.location.pathname)
    
 }
