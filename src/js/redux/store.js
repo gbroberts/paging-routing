a7cfccb057a5057831f3e91a861572bcff6146a4
Graham Roberts <grahamroberts31@gmail.com>
2021-10-27 15:46:51 -0600

    Adds redux dir to js dir and adds store.js

---
+++ src/js/redux/store.js
@@ -0,0 +1,15 @@
+let store = null
+
+// Create the store accessed by all our app components
+function createStore (data=[]){
+     if(store === null){
+         store = [...data]
+     }
+     return null
+}
+
+function getStore (){
+    return store
+}
+ 
+export {createStore, getStore}
