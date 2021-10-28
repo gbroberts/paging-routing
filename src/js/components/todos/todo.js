ac3058cfac4f24376f9b6057833bf6ea99e2c3b8
Graham Roberts <grahamroberts31@gmail.com>
2021-10-27 20:59:26 -0600

    Fixes innerText assignment of 'Completed'

--- src/js/components/todos/todo.js
+++ src/js/components/todos/todo.js
@@ -1,22 +1,22 @@
 import makeElement from "../../utils/makeElement";
-import {editIcon} from "./icons/editIcon";
-import {deleteIcon} from "./icons/deleteIcon";
 
 const todo = function ({id,category,title,isCompleted,startDate,startTime,endDate,endTime}) {
   const template = `       
   <li class="${category}" data-key="${id}">
-     <h2>${title}</h2>
-     <p>Due: ${endDate}</p>
-     <p class="complete"></p>
-     <p class="controls" data-key="${id}">
+    <div>
+      <h2>${title}</h2>
+      <p>Due: ${endDate}</p>
+      <p class="complete"></p>
+    </div>
+    <p class="controls" data-key="${id}">
       <button id="editTodo"><img src="./data/assets/editIcon.svg" alt="Edit"></button>
       <button id="deleteTodo"><img src="./data/assets/deleteIcon.svg" alt="Delete"></button>
-     </p>      
+    </p>      
   </li>        
   `;
   const templateHolder = makeElement(template);
   if(isCompleted){
-    templateHolder.querySelector('.complete').innerText.value = 'Completed';
+    templateHolder.querySelector('.complete').innerText = 'Completed';
     return templateHolder;
   }
   else{
