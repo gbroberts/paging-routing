e19b8f9506112f4f0352ad18af0fb789a1b6f357
Graham Roberts <grahamroberts31@gmail.com>
2021-11-20 17:02:48 -0700

    Adds store components

---
+++ src/redux/reducer.js
@@ -0,0 +1,24 @@
+import {getStore, updateStore} from './store'
+
+function reducer  (action){
+
+    switch(action.type){
+        case "delete":
+            // grabbing the current store
+            const store = getStore();
+            const index = action.payload.index;
+            const newStore = [...store.slice(0,index), ...store.slice(index+1)]
+            updateStore(newStore);
+            action.cb()
+            return "remove employee";
+        case "edit": 
+        return "edit employee";
+        case "add": 
+        return "add new employee";
+        default: return store
+    }
+
+}
+
+
+export default reducer 
